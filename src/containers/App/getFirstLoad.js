720dfd26ae82c9a258b2bd0bbdc521758c499212
Andrew Horn <arhorn@smcm.edu>
2019-07-18 02:46:29 -0500

    removed console.logs

--- src/containers/App/getFirstLoad.js
+++ src/containers/App/getFirstLoad.js
@@ -1,7 +1,5 @@
 const getFirstLoad = () => {
   const firstLoad = JSON.parse(localStorage.getItem('firstLoad'));
-  console.log(typeof firstLoad);
-  console.log(firstLoad);
   if (typeof firstLoad === 'boolean' && firstLoad === false) {
     return false;
   }
