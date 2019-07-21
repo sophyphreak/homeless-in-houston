fab69b071e960623092efd6ee1974bc95f64debd
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-21 02:13:49 -0500

    added pets radio button

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Gender from './Gender/Gender';
 import Age from './Age/Age';
 import LGBT from './LGBT/LGBT';
+import Pets from './Pets/Pets';
 
 const Filters = ({
   onFiltersRadioButtonClick,
-  filters: { gender, age, lgbt }
+  filters: { gender, age, lgbt, pets }
 }) => (
   <>
     <Gender onButtonClick={onFiltersRadioButtonClick} radioSelected={gender} />
     <Age onButtonClick={onFiltersRadioButtonClick} radioSelected={age} />
     <LGBT onButtonClick={onFiltersRadioButtonClick} radioSelected={lgbt} />
+    <Pets onButtonClick={onFiltersRadioButtonClick} radioSelected={pets} />
   </>
 );
 
--- src/components/Filters/Pets/Pets.js
+++ src/components/Filters/Pets/Pets.js
@@ -1,1 +1,14 @@
 // Use Basic Radio Button
+import React from 'react';
+import BasicRadioButtons from '../BasicRadioButtons/BasicRadioButtons';
+
+const Pets = props => (
+  <BasicRadioButtons
+    label="Do you have any pets?"
+    name="pets"
+    choices={['yes', 'no']}
+    {...props}
+  />
+);
+
+export default Pets;
