07267483cbce1816e83d0018761e570bdc60e7fe
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-21 01:39:07 -0500

    added LGBT radio button

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Gender from './Gender/Gender';
 import Age from './Age/Age';
+import LGBT from './LGBT/LGBT';
 
-const Filters = ({ onFiltersRadioButtonClick, filters: { gender, age } }) => (
+const Filters = ({
+  onFiltersRadioButtonClick,
+  filters: { gender, age, lgbt }
+}) => (
   <>
     <Gender onButtonClick={onFiltersRadioButtonClick} radioSelected={gender} />
-    <br />
     <Age onButtonClick={onFiltersRadioButtonClick} radioSelected={age} />
+    <LGBT onButtonClick={onFiltersRadioButtonClick} radioSelected={lgbt} />
   </>
 );
 
--- src/components/Filters/LGBT/LGBT.js
+++ src/components/Filters/LGBT/LGBT.js
@@ -1,1 +1,14 @@
 // Use Basic Radio Button
+import React from 'react';
+import BasicRadioButtons from '../BasicRadioButtons/BasicRadioButtons';
+
+const LGBT = props => (
+  <BasicRadioButtons
+    label="LGBT?"
+    name="lgbt"
+    choices={['yes', 'no']}
+    {...props}
+  />
+);
+
+export default LGBT;
