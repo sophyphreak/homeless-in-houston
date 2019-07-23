6e849eb7da39435eca7243516b9598610f72a16d
Andrew Horn <arhorn@smcm.edu>
2019-07-23 18:54:38 -0500

    added age props and two sample locations for testing purposes

--- src/containers/App/getInitialPlaceList/shelterList.js
+++ src/containers/App/getInitialPlaceList/shelterList.js
@@ -1,20 +1,25 @@
 const shelterList = [
   {
     name: "Star of Hope - Men's Development Center",
-    gender: ['male', 'nonbinary']
+    gender: ['male', 'nonbinary'],
+    age: ['all']
   },
   {
     name: 'Star of Hope Cornerstone Community',
-    gender: ['female', 'nonbinary']
+    gender: ['female', 'nonbinary'],
+    age: ['all']
   },
   {
     name: 'Salvation Army - Family Residence',
-    gender: ['female', 'nonbinary']
+    gender: ['female', 'nonbinary'],
+    age: ['all']
   },
-  { name: 'Covenant House', gender: ['all'] },
-  { name: 'Y.A.R.C.', gender: ['all'] },
-  { name: 'The Beacon', gender: ['all'] },
-  { name: 'VA McGovern Drop In Center', gender: ['all'] }
+  { name: 'Covenant House', gender: ['all'], age: ['all'] },
+  { name: 'Y.A.R.C.', gender: ['all'], age: ['18 to 24'] },
+  { name: 'The Beacon', gender: ['all'], age: ['65 and over'] },
+  { name: 'VA McGovern Drop In Center', gender: ['all'], age: ['all'] },
+  { name: 'Rothko Chapel', gender: ['all'], age: ['under 18'] },
+  { name: 'Menil Collection', gender: ['all'], age: ['all'] }
 ];
 
 export default shelterList;
