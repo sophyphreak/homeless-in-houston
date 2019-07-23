7dee0e7612a5837bbe12fddde759893197e9f312
Andrew Horn <arhorn@smcm.edu>
2019-07-23 14:56:08 -0500

    got rid of else statements

--- src/containers/App/getInitialPlaceList/getInitialPlaceList.js
+++ src/containers/App/getInitialPlaceList/getInitialPlaceList.js
@@ -1,29 +1,26 @@
 import shelterList from './shelterList';
 
 const getInitialPlaceList = (rawList = shelterList) =>
-  rawList.map(raw => {
-    const { name } = raw;
-    let gender = {};
-    if (raw.gender.includes('all')) {
+  rawList.map(rawPlace => {
+    const { name } = rawPlace;
+    const gender = {
+      nonbinary: false,
+      female: false,
+      male: false
+    };
+    if (rawPlace.gender.includes('nonbinary')) {
+      gender.nonbinary = true;
+    }
+    if (rawPlace.gender.includes('women')) {
+      gender.women = true;
+    }
+    if (rawPlace.gender.includes('men')) {
+      gender.male = true;
+    }
+    if (rawPlace.gender.includes('all')) {
       gender.male = true;
       gender.female = true;
       gender.nonbinary = true;
-    } else {
-      if (raw.gender.includes('men')) {
-        gender.male = true;
-      } else {
-        gender.male = false;
-      }
-      if (raw.gender.includes('women')) {
-        gender.female = true;
-      } else {
-        gender.female = false;
-      }
-      if (raw.gender.includes('nonbinary')) {
-        gender.nonbinary = true;
-      } else {
-        gender.nonbinary = false;
-      }
     }
     return { name, gender };
   });
