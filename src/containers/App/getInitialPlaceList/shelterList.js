f6d9b71e760a9dd1972d6a32fcb4d3433df3b59f
Andrew Horn <arhorn@smcm.edu>
2019-07-23 19:01:15 -0500

    added family props to shelterList

--- src/containers/App/getInitialPlaceList/shelterList.js
+++ src/containers/App/getInitialPlaceList/shelterList.js
@@ -2,24 +2,32 @@ const shelterList = [
   {
     name: "Star of Hope - Men's Development Center",
     gender: ['male', 'nonbinary'],
-    age: ['all']
+    age: ['all'],
+    family: false
   },
   {
     name: 'Star of Hope Cornerstone Community',
     gender: ['female', 'nonbinary'],
-    age: ['all']
+    age: ['all'],
+    family: true
   },
   {
     name: 'Salvation Army - Family Residence',
     gender: ['female', 'nonbinary'],
-    age: ['all']
+    age: ['all'],
+    family: true
   },
-  { name: 'Covenant House', gender: ['all'], age: ['all'] },
-  { name: 'Y.A.R.C.', gender: ['all'], age: ['18 to 24'] },
-  { name: 'The Beacon', gender: ['all'], age: ['65 and over'] },
-  { name: 'VA McGovern Drop In Center', gender: ['all'], age: ['all'] },
-  { name: 'Rothko Chapel', gender: ['all'], age: ['under 18'] },
-  { name: 'Menil Collection', gender: ['all'], age: ['all'] }
+  { name: 'Covenant House', gender: ['all'], age: ['all'], family: false },
+  { name: 'Y.A.R.C.', gender: ['all'], age: ['18 to 24'], family: false },
+  { name: 'The Beacon', gender: ['all'], age: ['65 and over'], family: false },
+  {
+    name: 'VA McGovern Drop In Center',
+    gender: ['all'],
+    age: ['all'],
+    family: false
+  },
+  { name: 'Rothko Chapel', gender: ['all'], age: ['under 18'], family: true },
+  { name: 'Menil Collection', gender: ['all'], age: ['all'], family: false }
 ];
 
 export default shelterList;
