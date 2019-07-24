9b0200574201bf85af5685c0b8562817f0e78987
Andrew Horn <arhorn@smcm.edu>
2019-07-23 20:15:37 -0500

    Added veteran filter

--- src/components/PlaceList/applyFilters/applyFilters.js
+++ src/components/PlaceList/applyFilters/applyFilters.js
@@ -4,15 +4,17 @@ import filterByAge from './filterByAge';
 import filterByFamily from './filterByFamily';
 import filterByLGBT from './filterByLGBT';
 import filterByPets from './filterByPets';
+import filterByVeteran from './filterByVeteran';
 
 const applyFilters = (placeList, filters) => {
-  const { gender, family, age, lgbt, pets } = filters;
+  const { gender, family, age, lgbt, pets, veteran } = filters;
   let localPlaceList = copy(placeList);
   localPlaceList = filterByGender(localPlaceList, gender, family);
   localPlaceList = filterByFamily(localPlaceList, family);
   localPlaceList = filterByAge(localPlaceList, age);
   localPlaceList = filterByLGBT(localPlaceList, lgbt);
   localPlaceList = filterByPets(localPlaceList, pets);
+  localPlaceList = filterByVeteran(localPlaceList, veteran);
   return localPlaceList;
 };
 
